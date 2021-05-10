a7c220fcdfa1967fca5229856effbf96e6329b4f
hetvikam_04 <59523596+HetVikam@users.noreply.github.com>
2021-05-10 20:27:49 +0530

    Update views.py

--- budgetproject/budget/views.py
+++ budgetproject/budget/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project
+from django.views.generic import CreatView
 
 
 def project_list(request):
@@ -10,3 +11,8 @@ def project_detail(request, project_slug):
 
     project = get_object_or_404(Project, slug=project_slug)
     return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
