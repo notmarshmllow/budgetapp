fc43f4c615a782c60e28f7d3fe162f86ec8edf94
hetvikam_04 <59523596+HetVikam@users.noreply.github.com>
2021-05-11 19:30:17 +0530

    Create script.js

---
+++ budgetproject/budget/static/js/script.js
@@ -0,0 +1,24 @@
+(function(){
+  
+  document.querySelector('#categoryInput').addEventListener("keydown', function(e){
+    if (e.keyCode != 13){
+      return;
+  }
+
+  e.preventDefault()
+  
+  var categoryName = this.value
+  
+  this.value = ''
+  
+  addNewCategory(categoryName) 
+  
+  updateCategoriesString()
+
+})
+
+function addNewCategory(name){
+
+}
+
+})()
